a1034291398a1618542c88513d99cbdd556996dd
Milagros932 <milinr29@gmail.com>
2020-08-21 17:06:35 -0500

    fix bug import

---
+++ src/config.js
@@ -0,0 +1,13 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDQYvhxho00hjo6AlP1o10NXE9VgfARxpM",
+    authDomain: "hdlkokoro.firebaseapp.com",
+    databaseURL: "https://hdlkokoro.firebaseio.com",
+    projectId: "hdlkokoro",
+    storageBucket: "hdlkokoro.appspot.com",
+    messagingSenderId: "388292856940",
+    appId: "1:388292856940:web:f28bbdafb9b0b0bf5d5c49",
+    measurementId: "G-E2YCRECKXV"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
