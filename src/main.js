8a89241db581452e89dafc4d5323a5ddc3b5cdb8
Milagros932 <milinr29@gmail.com>
2020-08-21 14:52:29 -0500

    add sass

--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
 // aqui va todo que tenga que ver con DOM
+const name= document.querySelector('#inputName')
+const phone= document.querySelector('#inputPhone')
+const email= document.querySelector('#inputEmail')
+const date= document.querySelector('#inputDate')
