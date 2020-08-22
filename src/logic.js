58cd083a4588421366523ba117cc4708bf407f71
Milagros932 <milinr29@gmail.com>
2020-08-21 21:56:13 -0500

    add firebase

--- src/logic.js
+++ src/logic.js
@@ -1,1 +1,26 @@
 // aqui van funciones puras o de firebase
+const db = firebase.firestore()
+
+const citas = () => db.collection("citas").get().then(function (querySnapshot) {
+    querySnapshot.forEach(function (doc) {
+        console.log(doc.id, " => ", doc.data());
+    });
+});
+const crearCita = (data) => {
+    console.log(data);
+    db.collection("citas").add({
+    nombre: data.name,
+    telefono: data.phone,
+    ine: data.ine,
+    email: data.email,
+    clinica: data.clinic,
+    doctor: data.doctor,
+    fecha: data.date,
+    acompañante: data.acomp,
+}).then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+})
+}
+
+
+export const firebaseFunctions = { citas, crearCita }
--- src/main.js
+++ src/main.js
@@ -1,51 +1,67 @@
 // aqui va todo que tenga que ver con DOM
+import { firebaseFunctions } from './logic.js'
 
-
-const btnContinuar = document.getElementById('btn-continue')
+const btnContinue = document.getElementById('btn-continue')
 const btnConfirm = document.getElementById('btn-confirm')
 const clinic = document.getElementById('inputClinic')
 const doctor = document.getElementById('inputDoctor')
-const data=[]
+const formInitial = document.getElementById('form-initial')
+const formEspecific = document.getElementById('form-especific')
+const dataResumen = document.getElementById('datos')
+const data = []
 
-
-btnContinuar.addEventListener('click', () => {
+btnContinue.addEventListener('click', () => {
     const nameValue = document.getElementById('inputName').value
     const phoneValue = document.getElementById('inputPhone').value
     const ineValue = document.getElementById('inputIne').value
     const emailValue = document.getElementById('inputEmail').value
-    
+
+    if (nameValue === null || nameValue === '') {
+        alert('llena los campos')
+    } else if (phoneValue === null || phoneValue === '') {
+        alert('llena los campos')
+    } else if (ineValue === null || ineValue === '') {
+        alert('llena los campos')
+        }else if (emailValue ===null || emailValue ===''){
+            alert('llena los campos')
+    } else {
+        data.push({ name: nameValue , phone: phoneValue , ine: ineValue ,  email: emailValue })
+        console.log(data);
+    }
 })
 
-
-let clinicValue=''
+let clinicValue = ''
 clinic.addEventListener('change', (e) => {
     e.preventDefault()
-   clinicValue =  clinic.value  
+    clinicValue = clinic.value
 })
 
-let doctorValue=''
-clinic.addEventListener('change', (e) => {
+let doctorValue = ''
+doctor.addEventListener('change', (e) => {
     e.preventDefault()
-    doctorValue =  doctor.value  
+    doctorValue = doctor.value
 })
 
 
 btnConfirm.addEventListener('click', (e) => {
-e.preventDefault()
-
-const dateValue = document.getElementById('inputDate').value
-const acompValue = document.getElementById('inputCheck').checked
-
-if((clinicValue !== undefined) || (doctorValue !== undefined)||(dateValue !== undefined)){
- console.log(clinicValue);
- }else {
-  //mostarr "llena este campo"
-  alert('no')
-}
-// if(doctorValue !== undefined){
-    
-// }
-
-
-
+    e.preventDefault()
+   
+    const dateValue = document.getElementById('inputDate').value
+    const acompValue = document.getElementById('inputCheck').checked
+    if (clinicValue === null || clinicValue === '') {
+        alert('llena los campos')
+    } else if (doctorValue === null || doctorValue === '') {
+        alert('llena los campos')
+    } else if (dateValue === null || dateValue === '') {
+        alert('llena los campos')
+    } else {
+        data.push({ clinic: clinicValue ,  doctor: doctorValue ,  date: dateValue , acomp: acompValue })
+        const result = {
+            ...data[0],
+            ...data[1],
+          }
+          console.log(result);
+        firebaseFunctions.crearCita(result)
+    }
 })
+
